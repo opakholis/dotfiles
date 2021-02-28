491755ab225efe0b6ca5e9e3912e145c1792f94f
Opa Kholis Majid <code@opakholis.space>
2021-02-28 09:56:26 +0700

    feat(polybar): support multiple monitor

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -10,5 +10,6 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch the bar
-polybar -q main -c "$DIR"/config.ini &
+# Launch Polybar, using default config location ~/.config/polybar/config
+polybar -q dpi -c "$DIR"/config.ini &
+polybar -q hdmi -c "$DIR"/config.ini &
