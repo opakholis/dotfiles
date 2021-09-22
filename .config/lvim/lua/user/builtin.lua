a848a9bf908b8b9960e0ffddaa3be2868bd01d77
Opa Kholis Majid <opakholis.id@gmail.com>
2021-09-22 21:54:07 +0700

    feat(lvim): add cmp config

--- .config/lvim/lua/user/builtin.lua
+++ .config/lvim/lua/user/builtin.lua
@@ -1,6 +1,47 @@
 local M = {}
 
 M.config = function()
+  -- CMP
+  -- =========================================
+  lvim.builtin.cmp.sources = {
+    { name = "nvim_lsp", max_item_count = 7 },
+    { name = "cmp_tabnine", max_item_count = 3 },
+    { name = "buffer", max_item_count = 3 },
+    { name = "path", max_item_count = 5 },
+    { name = "luasnip", max_item_count = 3 },
+    { name = "nvim_lua" },
+    { name = "calc" },
+    { name = "emoji" },
+    { name = "treesitter" },
+    { name = "crates" },
+  }
+  -- lvim.builtin.cmp.documentation.border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" }
+  lvim.builtin.cmp.documentation.border = { "┌", "─", "┐", "│", "┘", "─", "└", "│" }
+  lvim.builtin.cmp.formatting = {
+    format = function(entry, vim_item)
+      local cmp_kind = require("user.lsp_kind").cmp_kind
+      vim_item.kind = cmp_kind(vim_item.kind)
+      vim_item.menu = ({
+        buffer = "(Buffer)",
+        nvim_lsp = "(LSP)",
+        luasnip = "(Snip)",
+        treesitter = " ",
+        nvim_lua = "(NvLua)",
+        spell = " 暈",
+        emoji = "  ",
+        path = "  ",
+        calc = "  ",
+        cmp_tabnine = "  ",
+      })[entry.source.name]
+      vim_item.dup = ({
+        buffer = 1,
+        path = 1,
+        nvim_lsp = 0,
+      })[entry.source.name] or 0
+      return vim_item
+    end,
+  }
+
   -- Dashboard
   -- =========================================
   lvim.builtin.dashboard.active = true
