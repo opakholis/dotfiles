7eb1918ab32e1e8ff74d10ded2d624dc26dbd34d
Opa Kholis Majid <code@opakholis.space>
2021-10-14 22:28:11 +0700

    style(lvim): cleanup

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -24,31 +24,5 @@ lvim.lsp.automatic_servers_installation = true
 -- Language Specific
 -- =========================================
 require("user.null_ls").config()
+require("user.lsp_install").install()
 lvim.lsp.override = { "sumneko_lua", "tsserver" }
-for _, server_name in pairs(lvim.lsp.override) do
-  local lsp_installer_servers = require "nvim-lsp-installer.servers"
-  local server_available, requested_server = lsp_installer_servers.get_server(server_name)
-  if server_available then
-    if not requested_server:is_installed() then
-      if lvim.lsp.automatic_servers_installation then
-        requested_server:install()
-      else
-        return
-      end
-    end
-  end
-
-  local default_config = {
-    on_attach = require("lvim.lsp").common_on_attach,
-    on_init = require("lvim.lsp").common_on_init,
-    capabilities = require("lvim.lsp").common_capabilities(),
-  }
-
-  local status_ok, custom_config = pcall(require, "user/providers/" .. requested_server.name)
-  if status_ok then
-    local new_config = vim.tbl_deep_extend("force", default_config, custom_config)
-    requested_server:setup(new_config)
-  else
-    requested_server:setup(default_config)
-  end
-end
