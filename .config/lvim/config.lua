159ada60b9ebf080fa7bce18546b2d31da09590c
Opa Kholis Majid <opakholis.id@gmail.com>
2021-10-26 10:10:36 +0700

    revert(lvim): use smooth scrolling (again) but with other configuration

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -13,11 +13,13 @@ lvim.leader = "space"
 lvim.builtin.lua_dev = { active = true } -- change this to enable/disable folke/lua_dev
 lvim.builtin.presence = { active = false } -- change to true if you want discord presence
 lvim.builtin.persistence = { active = true } -- change to false if you don't want persistence
+lvim.builtin.neoscroll = { active = true } -- smooth scrolling
 require("user.builtin").config()
 require("user.neovim").config()
 require("user.plugins").config()
 require("user.which_key").config()
 
+lvim.builtin.notify.active = true
 lvim.lsp.diagnostics.virtual_text = false -- remove this line if you want to see inline errors
 lvim.lsp.automatic_servers_installation = false
 
