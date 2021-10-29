ddc688e55d7b5a2b1e63380475a9d8e27f21ed4a
Opa Kholis Majid <opakholis.id@gmail.com>
2021-10-29 13:38:20 +0700

    style(lvim): new indent_blankline setup

--- .config/lvim/lua/user/plugins.lua
+++ .config/lvim/lua/user/plugins.lua
@@ -75,7 +75,10 @@ M.config = function()
     {
       "lukas-reineke/indent-blankline.nvim",
       setup = function()
-        require("user.indent_blankline").setup()
+        vim.g.indent_blankline_char = "▏"
+      end,
+      config = function()
+        require("user.indent_blankline").config()
       end,
       event = "BufRead",
     },
