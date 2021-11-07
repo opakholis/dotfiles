28cf08d2e94503fe5f285d91a89b265fcee15631
Opa Kholis Majid <opakholis.id@gmail.com>
2021-11-07 10:08:04 +0700

    feat(lvim): add ts-rainbow config

---
+++ .config/lvim/lua/user/ts_rainbow.lua
@@ -0,0 +1,39 @@
+local M = {}
+
+M.config = function()
+  local status_ok, ts_rainbow = pcall(require, "nvim-treesitter.configs")
+  if not status_ok then
+    return
+  end
+
+  ts_rainbow.setup {
+    autopairs = { enable = true },
+    autotag = { enable = true },
+    ensure_installed = {
+      "html",
+      "css",
+      "json",
+      "tsx",
+      "php",
+      "python",
+      "yaml",
+      "lua",
+      "javascript",
+      "typescript",
+    }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+    highlight = { enable = true },
+    rainbow = {
+      enable = true,
+      extended_mode = false, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+      max_file_lines = nil, -- Do not enable for files with more than n lines, int
+      colors = {
+        "#8be9fd",
+        "#ffb86c",
+        "#ff79c6",
+      }, -- table of hex strings
+      -- termcolors = {} -- table of colour name strings
+    },
+  }
+end
+
+return M
