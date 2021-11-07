dce9260abe19d0424d208eb3e6fe677450cf4c16
Opa Kholis Majid <code@opakholis.space>
2021-11-07 10:00:04 +0700

    refactor(lvim): display color of "name" codes

--- .config/lvim/lua/user/colorizer.lua
+++ .config/lvim/lua/user/colorizer.lua
@@ -10,10 +10,13 @@ M.config = function()
     RGB = true, -- #RGB hex codes
     RRGGBB = true, -- #RRGGBB hex codes
     RRGGBBAA = true, -- #RRGGBBAA hex codes
+    names = true, -- "Name" codes like Blue
     rgb_fn = true, -- CSS rgb() and rgba() functions
     hsl_fn = true, -- CSS hsl() and hsla() functions
     css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
     css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+    -- Available modes: foreground, background
+    mode = "background", -- Set the display mode.
   })
 end
 
