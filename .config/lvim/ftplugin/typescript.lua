86b6b51ae6408f23b3a5cd8f8f7009d0f3caf188
Opa Kholis Majid <code@opakholis.space>
2022-09-25 15:51:59 +0700

    style: move leftwm to a separate repo

--- .config/lvim/ftplugin/typescript.lua
+++
@@ -1,8 +0,0 @@
-local formatters = require "lvim.lsp.null-ls.formatters"
-formatters.setup {
-  {
-    exe = "prettierd",
-    filetypes = { "typescript" },
-    args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
-  },
-}
